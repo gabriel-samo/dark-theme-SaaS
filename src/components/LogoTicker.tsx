6247a2d3b6e359d127d71d7731da68a8948e7127
Gabriel Samoylov <gsamo122@gmail.com>
2024-08-23 16:56:49 +0300

    complete: Logo Ticker Section

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -1,9 +1,12 @@
-import acmeLogo from "../assets/images/acme.png";
-import quantumLogo from "../assets/images/quantum.png";
-import echoLogo from "../assets/images/echo.png";
-import celestialLogo from "../assets/images/celestial.png";
-import pulseLogo from "../assets/images/pulse.png";
+"use client";
+
+import Image from "next/image";
 import apexLogo from "../assets/images/apex.png";
+import acmeLogo from "../assets/images/acme.png";
+import pulseLogo from "../assets/images/pulse.png";
+import echoLogo from "../assets/images/echo.png";
+import quantumLogo from "../assets/images/quantum.png";
+import celestialLogo from "../assets/images/celestial.png";
 
 const images = [
   { src: acmeLogo, alt: "Acme Logo" },
@@ -11,9 +14,29 @@ const images = [
   { src: echoLogo, alt: "Echo Logo" },
   { src: celestialLogo, alt: "Celestial Logo" },
   { src: pulseLogo, alt: "Pulse Logo" },
-  { src: apexLogo, alt: "Apex Logo" },
+  { src: apexLogo, alt: "Apex Logo" }
 ];
 
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24 ">
+      <div className="container">
+        <h2 className="text-xl text-center text-white/70 ">
+          Trusted by the most innovative teams
+        </h2>
+        <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
+          <div className="flex gap-16">
+            {images.map(({ src, alt }) => (
+              <Image
+                src={src}
+                alt={alt}
+                key={alt}
+                className="flex-none h-8 w-auto"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
