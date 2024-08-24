438475930b8c1e48acc5f2febe6ea03d016e5c41
Gabriel Samoylov <gsamo122@gmail.com>
2024-08-25 00:14:36 +0300

    complete: FAQs Animations

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -3,6 +3,7 @@ import clsx from "clsx";
 import { useState } from "react";
 import PlusIcon from "@/assets/icons/plus.svg";
 import MinusIcon from "@/assets/icons/minus.svg";
+import { AnimatePresence, motion } from "framer-motion";
 
 const items = [
   {
@@ -44,7 +45,29 @@ const AccordionItem = ({
         <span className="flex-1 text-lg font-bold">{question}</span>
         {isOpen ? <MinusIcon /> : <PlusIcon />}
       </div>
-      <div className={clsx("mt-4", { hidden: !isOpen })}>{answer}</div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{
+              opacity: 0,
+              height: 0,
+              marginTop: 0
+            }}
+            animate={{
+              opacity: 1,
+              height: "auto",
+              marginTop: "16px"
+            }}
+            exit={{
+              opacity: 0,
+              height: 0,
+              marginTop: 0
+            }}
+          >
+            {answer}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
