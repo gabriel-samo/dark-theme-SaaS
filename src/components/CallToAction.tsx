53a8008947ce89fb323147e3c823132252122809
Gabriel Samoylov <gsamo122@gmail.com>
2024-08-25 00:30:53 +0300

    complete: Call To Action Animation

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,21 +1,39 @@
+"use client";
+import Image from "next/image";
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+
 import helixImage from "@/assets/images/helix2.png";
 import emojiStartImage from "@/assets/images/emojistar.png";
-import Image from "next/image";
 
 export const CallToAction = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start end", "end end"]
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [40, -40]);
+
   return (
-    <div className="bg-black text-white py-[72px] text-center overflow-x-clip">
+    <div
+      className="bg-black text-white py-[72px] text-center overflow-x-clip"
+      ref={containerRef}
+    >
       <div className="container max-w-xl relative">
-        <Image
-          src={helixImage}
-          alt=""
-          className="absolute top-6 left-[calc(100%+36px)]"
-        />
-        <Image
-          src={emojiStartImage}
-          alt=""
-          className="absolute -top-[120px] right-[calc(100%+24px)]"
-        />
+        <motion.div style={{ translateY }}>
+          <Image
+            src={helixImage}
+            alt=""
+            className="absolute top-6 left-[calc(100%+36px)]"
+          />
+        </motion.div>
+        <motion.div style={{ translateY }}>
+          <Image
+            src={emojiStartImage}
+            alt=""
+            className="absolute -top-[120px] right-[calc(100%+24px)]"
+          />
+        </motion.div>
         <h2 className="font-bold text-5xl tracking-tighter sm:text-6xl">
           Get instant access
         </h2>
